579c7bc9d1e5de25118e1de1f9ad6b4dc5905c00
Mohamed Moamen <mohamedmoamen8@gmail.com>
2026-04-07 23:36:45 +0200

    fix duplicate authentication middleware

--- src/middleware/auth.middeleware.js
+++ src/middleware/auth.middeleware.js
@@ -3,38 +3,8 @@ import dotenv from "dotenv";
 import redisClient from "../utils/redisClient.js";
 
 dotenv.config({
-  path: "./config/.env.development"
+  path: "./config/.env.development",
 });
-
-
-export const authentication = (req, res, next) => {
-  try {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      throw new Error("Authorization header is required", {
-        cause: { status: 400 },
-      });
-    }
-
-    const token = authHeader.split(" ")[1];
-
-    if (!token) {
-      throw new Error("Token is required", {
-        cause: { status: 400 },
-      });
-    }
-
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-
-    req.user = decoded; 
-
-    next();
-  } catch (error) {
-    next(error);
-  }
-};
-
 
 export const verifyTokenMiddleware = async (req, res, next) => {
   try {
@@ -50,14 +20,13 @@ export const verifyTokenMiddleware = async (req, res, next) => {
       return res.status(401).json({ message: "Token missing" });
     }
     const isBlackListed = await redisClient.get(`blacklist_${token}`);
-if (isBlackListed) {
-  return res.status(401).json({ message: "Token is blacklisted" });
-}
+    if (isBlackListed) {
+      return res.status(401).json({ message: "Token is blacklisted" });
+    }
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
 
-    req.user = decoded; 
+    req.user = decoded;
     next();
-
   } catch (err) {
     return res.status(401).json({ message: "Invalid or expired token" });
   }
@@ -78,7 +47,6 @@ export const authentication = async (req, res, next) => {
       throw new Error("Token is required", { cause: { status: 400 } });
     }
 
-    
     const isBlacklisted = await redisClient.get(`blacklist_${token}`);
     if (isBlacklisted) {
       throw new Error("Token has been invalidated, please login again", {
